f7b28ac56860d14299388336615c5df3fe89f6b0
Andy Goldschmidt <andygoldschmidt@me.com>
2012-12-24 00:03:30 +0100

    Moeglichkeit fuer Initialisierung mit None.

--- footballmetrics/dataloader.py
+++ footballmetrics/dataloader.py
@@ -39,12 +39,15 @@ class DataLoader(object):
 
 
 class DataHandler(object):
-    def __init__(self, games_df, standings_df):
-        self._games_df = games_df
-        self._standings_df = standings_df
-        if self._standings_df.index.name != 'Team':
-            self._standings_df.set_index('Team', inplace=True)
-        self._check_integrity()
+    def __init__(self, games_df=None, standings_df=None):
+        if games_df is not None:
+            self._games_df = games_df
+        if standings_df is not None:
+            self._standings_df = standings_df
+            if self._standings_df.index.name != 'Team':
+                self._standings_df.set_index('Team', inplace=True)
+        if games_df is not None and standings_df is not None:
+            self._check_integrity()
 
     def _check_integrity(self):
         '''Checks integrity of the two provided Data Frames.'''
@@ -53,18 +56,27 @@ class DataHandler(object):
         diff_teams = teams_games - teams_standings
         if diff_teams != set([]):
             raise ValueError('Found differences in available teams.')
-        
 
     def get_teams(self):
         '''Returns all teams from standings Data Frame.'''
-        teams = sorted(self._standings_df.index)
+        try:
+            teams = sorted(self._standings_df.index)
+        except AttributeError:
+            raise AttributeError('Standings Data Frame not set. Use set_standings_df().')
         return teams
 
     def get_margins(self):
         '''Returns the margin of victory of every game in the game Data Frame.'''
-        margins = self._games_df['HomeScore'] - self._games_df['AwayScore']
+        try:
+            margins = self._games_df['HomeScore'] - self._games_df['AwayScore']
+        except AttributeError:
+            raise AttributeError('Games Data Frame not set. Use set_games_df().')
         return margins
 
     def get_wins(self):
         '''Returns the number of wins for each team.'''
-        return self._standings_df['Win']
+        try:
+            wins = self._standings_df['Win']
+        except AttributeError:
+            raise AttributeError('Standings Data Frame not set. Use set_standings_df().')
+        return wins
