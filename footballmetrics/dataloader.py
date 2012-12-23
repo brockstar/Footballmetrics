bbffa933e7f1825cfdcd651e2acfaea4e16ead09
Andy Goldschmidt <andygoldschmidt@me.com>
2012-12-23 19:27:16 +0100

    DataHandler-Klasse begonnen und von DataLoader getrennt.

--- footballmetrics/dataloader.py
+++ footballmetrics/dataloader.py
@@ -10,18 +10,61 @@ import pandas.io.sql as pd_sql
 
 class DataLoader(object):
     def __init__(self):
-        pass
+        self._df = None
 
-    def load_sqlite(self, db_path, query):
+    def load_sqlite(self, db_path, query, index=None):
+        '''
+        Loads data from a local SQLite database. The database needs
+        to be located in *db_path*, *query* is the submitted SQL query.
+        '''
         if not os.path.isfile(db_path):
             raise IOError('Database does not exist.')
         con = sqlite3.connect(db_path)
-        self.df = pd_sql.read_frame(query, con)
+        if index is None:
+            self._df = pd_sql.read_frame(query, con)
+        else:
+            self._df = pd_sql.read_frame(query, con, index_col=index)
         con.close()
-        return self.df
+        return self._df
 
     def load_csv(self, filename):
+        '''
+        Loads the data from a comma-separated values file (CSV).
+        The file needs to be located at *filename*.
+        '''
         if not os.path.isfile(filename):
             raise IOError('File not found.')
-        self.df = pd.read_csv(filename)
-        return self.df
+        self._df = pd.read_csv(filename)
+        return self._df
+
+
+class DataHandler(object):
+    def __init__(self, games_df, standings_df):
+        self._games_df = games_df
+        self._standings_df = standings_df
+        if self._standings_df.index.name != 'Team':
+            self._standings_df.set_index('Team', inplace=True)
+        self._check_integrity()
+
+    def _check_integrity(self):
+        '''Checks integrity of the two provided Data Frames.'''
+        teams_games = set(self._games_df['HomeTeam']) | set(self._games_df['AwayTeam'])
+        teams_standings = set(self.get_teams())
+        diff_teams = teams_games - teams_standings
+        if diff_teams != set([]):
+            raise ValueError('Found differences in available teams.')
+        
+
+    def get_teams(self):
+        '''Returns all teams from standings Data Frame.'''
+        teams = sorted(self._standings_df.index)
+        return teams
+
+    def get_margins(self):
+        '''Returns the margin of victory of every game in the game Data Frame.'''
+        margins = self._games_df['HomeScore'] - self._games_df['AwayScore']
+        return margins
+
+    def get_wins(self):
+        '''Returns the number of wins for each team.'''
+        return self._standings_df['Win']
--- testdataloader.py
+++ testdataloader.py
@@ -14,7 +14,7 @@ class TestDataLoader(unittest.TestCase):
                      'HomeScore': [27, 17, 31, 3, 14, 42],
                      'AwayScore': [13, 41, 28, 10, 13, 41]}
         self.games = pd.DataFrame(gamesdict)
-    
+        
     def test_load_sqlite(self):
         dl = fm_dl.DataLoader()
         df = dl.load_sqlite('test.db', 'select * from games')
@@ -29,6 +29,38 @@ class TestDataLoader(unittest.TestCase):
     def test_load_sqlite_invalid_query(self):
         dl = fm_dl.DataLoader()
         self.assertRaises(sqlite3.OperationalError, dl.load_sqlite, 'test.db', 'select * from invalid_table')
+
+    def test_load_csv(self):
+        dl = fm_dl.DataLoader()
+        df = dl.load_csv('test_dataloader.csv')
+        for key in df:
+            series = df[key] == self.games[key]
+            self.assert_(list(series.unique()) == [True])
+
+    def test_load_csv_nofile(self):
+        dl = fm_dl.DataLoader()
+        self.assertRaises(IOError, dl.load_csv, 'nofile.csv')
+
+
+class TestDataHandler(unittest.TestCase):
+    def setUp(self):
+        dl = fm_dl.DataLoader()
+        self.games = dl.load_sqlite('test.db', 'select * from games')
+        self.standings = dl.load_sqlite('test.db', 'select * from standings', index='Team')
+        self.dh = fm_dl.DataHandler(self.games, self.standings)
+   
+    def test_get_teams(self):
+        self.assertEqual(self.dh.get_teams(), ['A', 'B', 'C', 'D'])
+
+    def test_get_margins(self):
+        margins = self.games['HomeScore'] - self.games['AwayScore']
+        series = self.dh.get_margins() == margins
+        self.assert_(list(series.unique()) == [True])
+
+    def test_get_wins(self):
+        wins = self.standings['Win']
+        series = wins == self.dh.get_wins()
+        self.assertEqual(list(series.unique()), [True])
 
 
 if __name__ == '__main__':
